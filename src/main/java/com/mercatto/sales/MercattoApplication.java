ee21cac7e18e2199572a8c7ec7eecd30e9f05515
Alberto Ordaz <albert0rz1213@gmail.com>
2025-06-01 21:05:00 -0600

    feat: :construction: init

---
+++ src/main/java/com/mercatto/sales/MercattoApplication.java
@@ -0,0 +1,13 @@
+package com.mercatto.sales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercattoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercattoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mercatto/sales/MercattoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercatto.sales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercattoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
